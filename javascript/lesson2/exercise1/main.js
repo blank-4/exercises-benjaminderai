90ad3c327a411791a5b0043fcf437d0d44b01903
Benjamin Derai <derai.benjamin@gmail.com>
2018-11-26 16:42:55 +0100

    js-lesson2

---
+++ javascript/lesson2/exercise1/main.js
@@ -0,0 +1,8 @@
+var heure = 23
+if (heure < 10) {
+    console.log("C'est le matin");
+  } else if (heure < 20){
+    console.log("C'est l'après-midi");
+  } else {
+    console.log("c'est le soir");
+  }
