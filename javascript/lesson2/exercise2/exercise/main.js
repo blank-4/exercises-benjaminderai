bff0758fe46bd8911d3d74cbb3bb10cf255833c4
Benjamin Derai <derai.benjamin@gmail.com>
2018-11-26 17:58:55 +0100

    exercices

---
+++ javascript/lesson2/exercise2/exercise/main.js
@@ -0,0 +1,15 @@
+for ( var i = 0 ; i < 100 ; i++ ) {
+  console.log ("je compte jusqu'a " + i);
+}
+
+for ( var i = 0 ; i < 100 ; i = i + 2 ) {
+  console.log ("je compte jusqu'a " + i);
+}
+
+for ( var i = 100 ; i > 0 ; i = i - 2 ) {
+  console.log ("je compte jusqu'a " + i);
+}
+
+for ( var i = 100 ; i > 0 ; i = i - 2 ) {
+  console.log ("je compte jusqu'a " + i);
+}
---
+++ javascript/lesson2/exercise4/exercise/main.js
@@ -0,0 +1,17 @@
+var position = "Belleville";
+
+switch(position) {
+  case "mairie des lilas":
+     console.log ("tu est sur la ligne 11 ou la ligne 2");
+      break;
+  case "belleville":
+     console.log ("tu est sur la ligne 11");
+  case "mairie des lilas":
+      console.log ("tu est sur la ligne 11 ou la ligne 2");
+       break;
+  case "pere lachaise":
+      console.log ("tu est sur la ligne 3 ou 2");
+       break;
+  default:
+      console.log ("Je ne sais pas ou tu es.");
+}
