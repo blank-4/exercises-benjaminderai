32bce6c77998b4ed1ddaa176a001b41b1bdf651a
Benjamin Derai <derai.benjamin@gmail.com>
2018-11-26 14:18:28 +0100

    js-ex2

---
+++ javascript/lesson1/exercise2/main.js
@@ -0,0 +1,40 @@
+var nb1 = 2;
+
+var nb2 = 3;
+
+var nb3 = nb1+nb2;
+
+console.log ("Le resultat de " + nb1 + " + " + nb2 + " est " + nb3);
+
+var a1 = 4;
+
+var b2 = 3;
+
+var c3 = a1-b2;
+
+console.log ("Le resultat de " + a1 + " - " + b2 + " est " + c3);
+
+var d4 = 7;
+
+var e5 = 9;
+
+var f6 = d4*e5;
+
+console.log ("Le resultat de " + d4 + " * " + e5 + " est " + f6);
+
+var g7 = 12;
+
+var h8 = 6;
+
+var i9 = g7/h8;
+
+console.log ("Le resultat de " + g7 + " / " + h8 + " est " + i9);
+
+
+var k10 = 12;
+
+var l11 = 6;
+
+var m12 = k10%l11;
+
+console.log ("Le resultat de " + g7 + " % " + h8 + " est " + i9);
