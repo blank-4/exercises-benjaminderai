7ec24cacf9262fddaf9c22ec7629000ebe100d67
Benjamin Derai <derai.benjamin@gmail.com>
2018-11-26 11:28:35 +0100

    js ex1

---
+++ javascript/lesson1/exercise1/main.js
@@ -0,0 +1,17 @@
+var toto = "bonjour";
+console.log(toto);
+
+toto = 1 + 1;
+console.log(toto);
+
+toto = 1 - 1;
+console.log(toto);
+
+toto = 1 * 1;
+console.log(toto);
+
+toto = 1 / 1;
+console.log(toto);
+
+toto = 1 % 1;
+console.log(toto);
