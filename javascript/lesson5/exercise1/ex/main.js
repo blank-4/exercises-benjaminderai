8a59985f4e5fecb0b54971c025764ef4d3b8a626
Benjamin Derai <derai.benjamin@gmail.com>
2018-12-06 16:42:53 +0100

    ex1-lesson5

--- javascript/lesson5/exercise1/ex/main.js
+++ javascript/lesson5/exercise1/ex/main.js
@@ -1,1 +1,21 @@
-var y = document.getElementById("texte").innerHTML= "lorem ipsum    ";
+var x = document.getElementById("texte");
+
+x.innerHTML = "lorem ipsum";
+function afficher () {
+    document.getElementById("texte").style.display = "block";
+    x.style.color = couleur();
+
+}
+
+function cacher (){
+    x.style.display = "none";
+}
+
+function couleur (){
+    var couleur = ["#9932CC","#20B2AA","#FF4500"];
+    var index = Math.floor (Math.random()* couleur.length);
+        console.log(index.couleur);
+    var macolor = couleur[index];
+    return macolor;
+
+}
